bba6b071d8a73b56ce78b6491e033c38f194a808
ixsvf <ivofernandes12@gmail.com>
2026-06-13 16:51:57 +0100

    Update FluxMonitorDbContext.cs

--- back-end/FluxMonitor.Infrastructure/Data/FluxMonitorDbContext.cs
+++ back-end/FluxMonitor.Infrastructure/Data/FluxMonitorDbContext.cs
@@ -20,7 +20,7 @@ public class FluxMonitorDbContext : DbContext
             Id = Guid.Parse("a1b2c3d4-e5f6-7a8b-9c0d-1e2f3a4b5c6d"),
             Username = "admin",
             Email = "admin@fluxmonitor.com",
-            // Hash gerado previamente para a string "admin123" usando BCrypt
+            // Hash "admin123" BCrypt
             PasswordHash = "$2a$11$mC/7D3bV7KzS6gM7rMhRre7VbLmWXv8A1O5JmWqE2qE5i6z3c2q1G", 
             UserGroup = "admin"
         });
